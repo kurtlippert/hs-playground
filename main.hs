42bd2aecc2ca64631fe7d7cfa556e31c4e80f840
Kurt Lippert <kurtlippert@gmail.com>
2021-01-21 01:52:29 +0000

    Initial commit

---
+++ main.hs
@@ -0,0 +1,25 @@
+import Data.List
+import qualified Data.IntMap as IntMap
+
+doIt :: [a] -> [(a,a)]
+doIt [] = []
+doIt (x:[]) = []
+doIt (x:xs) =
+  ((<>) <$> fmap ((,) x) <*> doIt) xs
+
+doIfNotEmpty :: [a] -> ([a] -> [(a,a)]) -> Maybe ([(a,a)])
+doIfNotEmpty xs f =
+  if null xs then Nothing else Just (f xs)
+
+
+doubleSmallNumber x =
+  if x > 100
+    then x
+    else x*2
+
+foreverList thingToPrepend takeThisMany = 
+  take takeThisMany $ thingToPrepend : [1..]
+
+main :: IO ()
+main = do
+  print $ doIt [1,2,3]
