f1eac6237bc5e0c5d1b760cd5e6fd51a5fc35d67
Kurt Lippert <kurtlippert@gmail.com>
2021-01-27 20:40:11 -0500

    vscode ghci and ghcid integration

--- Main.hs
+++ Main.hs
@@ -1,9 +1,8 @@
-import Data.List
-import qualified Data.IntMap as IntMap
+module Main where
 
 doIt :: [a] -> [(a,a)]
 doIt [] = []
-doIt (x:[]) = []
+doIt [_] = []
 doIt (x:xs) =
   ((<>) <$> fmap ((,) x) <*> doIt) xs
 
@@ -11,15 +10,20 @@ doIfNotEmpty :: [a] -> ([a] -> [(a,a)]) -> Maybe ([(a,a)])
 doIfNotEmpty xs f =
   if null xs then Nothing else Just (f xs)
 
-
+-- >>> doubleSmallNumber 8
+-- 16
+--
+doubleSmallNumber :: (Ord a, Num a) => a -> a
 doubleSmallNumber x =
   if x > 100
     then x
     else x*2
 
+foreverList :: (Num a, Enum a) => a -> Int -> [a]
 foreverList thingToPrepend takeThisMany = 
   take takeThisMany $ thingToPrepend : [1..]
 
+reverseForeverList :: (Num a, Enum a) => a -> Int -> [a]
 reverseForeverList thingToPrepend takeThisMany =
   reverse $ foreverList thingToPrepend takeThisMany
 
@@ -32,11 +36,11 @@ padLeft size collection padWith =
   concat [take (size - length collection) $ cycle padWith, collection]
 
 len :: (Num b) => [a] -> b
-len = foldr (\x -> (+) 1) 0
+len = foldr (\_ -> (+) 1) 0
 
 len2 :: (Num b) => [a] -> b
 len2 [] = 0
-len2 (head:tail) = (len2 tail) + 1
+len2 (_:xs) = (len2 xs) + 1
 
 calcBmis :: (RealFloat a) => [(a, a)] -> [a]
 calcBmis xs = 
